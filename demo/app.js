59ce60f06a661e718a2302a60f4ba8e186d87fb4
gokulkrishh <krishnangokul9@gmail.com>
2015-05-29 13:53:12 +0530

    Updated

--- demo/app.js
+++ demo/app.js
@@ -1,23 +1,37 @@
 (function () {
 	'use strict';
 
-	console.log('Came ----->');
-
 	//If serviceWorker supported, then register it
 	//Else warn use that serviceWorker is not supported
 	if ('serviceWorker' in navigator) {
-		console.log('Supports service worker --------->');
-
-    navigator.serviceWorker.register('/serviceWorker.js') //Registering service worker
+    navigator.serviceWorker.register('./serviceWorker.js') //Registering service worker
     .then(initializeFunz);
 	}
 	else {
 		console.warn('Your browser does not support service worker, please update your browser');
 	}
 
+	var btn = document.getElementById('subscribe');
+	//Adding click event to btn
+	btn.addEventListener('click', function () {
+		console.log('Clicked ------->');
+	});
 
+	//After service worker register, check for notification support
 	function initializeFunz() {
-		console.log('Initialized -------->');
+		console.log('Initialized -------->', Notification.permission);
+
+		//Check for Notification permission
+		if (Notification.permission === 'denied') {
+			alert('User blocked the notification');
+			return false;
+		}
+
+		//Check for push notification support
+		if (!('pushManager' in window)) {
+			alert('Push notification is not supported');
+			return false;
+		}
 	}
 
 })();
